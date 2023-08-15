b107d700a3a7379f6c736029d1f5845bf4650d0e
Maurizio Pietrantuono <maurizio.pietrantuono@gmail.com>
2023-08-15 11:51:54 +0100

    OK with query

--- src/main/java/com/maurizio/QueryTable.java
+++ src/main/java/com/maurizio/QueryTable.java
@@ -6,10 +6,11 @@ import com.amazonaws.services.lambda.runtime.RequestHandler;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.Map;
 
-public class UpdateTable implements RequestHandler<Map<String, String>, Void> {
+public class QueryTable implements RequestHandler<Map<String, String>, Void> {
     private String dbName = System.getenv("RDS_DB_NAME");
     private String userName = System.getenv("RDS_USERNAME");
     private String password = System.getenv("RDS_PASSWORD");
@@ -24,14 +25,16 @@ public class UpdateTable implements RequestHandler<Map<String, String>, Void> {
             Connection conn = DriverManager.getConnection(jdbcUrl, userName, password);
             Statement stmt = conn.createStatement();
             logger.log("Connections successful");
-            String createTable = "INSERT INTO PETS ("
+            String query = "SELECT"
                     + "PetName, "
                     + "OwnerName, "
                     + "Species, "
-                    + "Age) "
-                    +"VALUES ('Travis', 'Maurizio', 'Cockatiel', 3)";
-            stmt.executeUpdate(createTable);
-            logger.log("table updated");
+                    + "Age "
+                    + " FROM PETS";
+            ResultSet resultSet = stmt.executeQuery(query);
+            while (resultSet.next()) {
+                logger.log(resultSet.getString("PetName"));
+            }
         } catch (Exception e) {
             logger.log("ERROR! " + e.getMessage());
         }
